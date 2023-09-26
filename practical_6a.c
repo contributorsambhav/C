80f060393a3321b096bdcefb7d381bc415751f54
contributorsambhav <Sambhav511974@gmail.com>
2023-09-26 15:24:06 +0530

    To find area of rectanglr and area and ciecumference of circle

---
+++ practical_6a.c
@@ -0,0 +1,22 @@
+// a) Write a program to print the area as the parameter of the rectangle.
+
+// b ) Write a program to print the area and parameter of circle.
+
+#include <stdio.h>
+
+int main(){
+int l,b,a;
+
+printf("Enter the length: ");
+scanf("%d",&l);
+
+printf("Enter the breadth: ");
+scanf("%d",&b);
+
+a=l*b;
+printf("The area of rectangle is %d",a);
+
+
+    return 0 ;
+
+}
---
+++ practical_6b.c
@@ -0,0 +1,22 @@
+// a) Write a program to print the area as the parameter of the rectangle.
+
+// b ) Write a program to print the area and parameter of circle.
+
+#include <stdio.h>
+
+int main(){
+int r,c,a;
+const pi=3.14;
+
+printf("Enter the radius: ");
+scanf("%d",&r);
+
+c=2*pi*r;
+printf("The circumference of circle is %d\n",c);
+
+a=pi*r*r;
+printf("The area of circle is %d",a);
+
+
+    return 0 ;
+}
