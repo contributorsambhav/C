7e08adc5b89158cab62de31232dd62d6db13f6ec
contributorsambhav <Sambhav511974@gmail.com>
2023-09-29 13:27:04 +0530

    Ternary operator

---
+++ ternary.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+
+int main(){
+
+
+    int myage = 15 ;
+
+    int votestate = (myage>=18) ? 1 :0 ;
+
+    printf("%d",votestate);
+
+    return 0;
+
+
+}
