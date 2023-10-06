84ea3295421b0f052a74d06acbbc8a9c08208297
contributorsambhav <Sambhav511974@gmail.com>
2023-10-06 15:50:41 +0530

    ternary

--- ternary.c
+++ ternary.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-#include<math.h>
-#include<stdbool.h>
 
 int main(){
 
 
-    int myage = 15 ;
+    int myage ;
+    scanf("%d",&myage);
 
-    int votestate = (myage>=18) ? 1 :0 ;
+    int votestate = (myage>=18)? 1 :0 ;
 
     printf("%d",votestate);
 
