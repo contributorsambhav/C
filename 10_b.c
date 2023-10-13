8d32db6b79f23bccf2b46971418996819f38fee5
contributorsambhav <Sambhav511974@gmail.com>
2023-10-13 14:46:11 +0530

    formatted

--- 10_b.c
+++ 10_b.c
@@ -1,17 +1,20 @@
 // a) Write a program to print the pattern 1
-// b) 
+// b)
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-     int n; 
+    int n;
     printf("Enter the number of rows : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            printf("%d",i);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", i);
         }
         printf("\n");
     }
