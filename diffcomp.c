20350f64b028370034b50dd627f34d28bf81fbd5
contributorsambhav <Sambhav511974@gmail.com>
2023-12-07 17:49:32 +0530

    comparing pointers

---
+++ diffcomp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    char c;
+    c = 'a';
+    char *p = &c;
+    char arr [5]= "aello";
+    char *str  =arr;
+
+
+    if(*p == 97){
+        printf("true \n");
+    }
+
+    if(str[0] = 97){
+        printf("alsotrue");
+    }
+
+    
+}
